4228ff3c55dce0f1e954715c2a9acc12ad0da86c
gabor.kovesdan@gmail.com <gabor.kovesdan@gmail.com@c91689b2-cc56-07db-5521-d62647d53ccb>
2012-08-17 06:17:07 +0000

    - Add a script that processes JPEG 2000 images and creates a   video stream of them - Add a script that forwards this stream to the streaming server

---
+++ bin/stream_consumer.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+PIPE=stream.ogg
+STREAMHOST=localhost
+STREAMPORT=4551
+STREAMPASS=changeme
+MOUNTPOINT=helio.ogg
+NAME="Unnamed Video Stream"
+DESC="JHelioviewer Video Channel"
+
+usage()
+{
+	echo "$0 [-H host] [-p port] [-l pass] [-m mount_point] [-s pipe]"
+	echo "$0 -h"
+}
+
+while getopts ":d:hH:l:m:n:p:s:" opt; do
+	case ${opt} in
+	d)
+		DESC="${OPTARG}"
+		;;
+	h)
+		usage
+		exit 0
+		;;
+	H)
+		STREAMHOST=${OPTARG}
+		;;
+	l)
+		STREAMPASS=${OPTARG}
+		;;
+	m)
+		MOUNTPOINT=${OPTARG}
+		;;
+	n)
+		NAME="${OPTARG}"
+		;;
+	p)
+		STREAMPORT=${OPTARG}
+		;;
+	s)
+		PIPE=${OPTARG}
+		;;
+	\?)
+		echo "Invalid option: -${OPTARG}" >&2
+		usage
+		exit 1
+		;;
+	:)
+		echo "Option -${OPTARG} requires an argument." >&2
+		usage
+		exit 1
+		;;
+	esac
+done
+
+#
+# Some sanity check on the parameters follow
+#
+
+if [ ! -p ${PIPE} ]
+then
+	echo "Specified named pipe ${PIPE} does not exist." >&2
+	exit 2
+fi
+
+_STREAMPORT=`echo ${STREAMPORT} | grep -oE '^[[:digit:]]+$'`
+
+if [ -z ${_STREAMPORT} ]
+then
+	echo "Invalid stream port specification. It can only contain digits." >&2
+	exit 2
+fi
+
+#
+# Main loop to read out video data from the named pipe and send it to
+# the streaming server
+#
+
+while :
+do
+	oggfwd -n "${NAME}" -d "${DESC}" ${STREAMHOST} ${STREAMPORT} ${STREAMPASS} /${MOUNTPOINT} < ${PIPE}
+done
---
+++ bin/stream_producer.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+#
+# Default values for variables
+#
+KAKADUPATH=./bin
+SRCDIR=img
+TMPDIR=./tmp
+SECPERIMG=3
+RESOLUTION=1024x1024
+
+usage()
+{
+	echo "$0 [-d sourcedir] [-K kakadu_path] [-n sec_per_img] [-r resolution] [-t tmpdir]"
+	echo "$0 -h"
+}
+
+while getopts ":d:hK:n:r:t:" opt; do
+        case ${opt} in
+	h)
+		usage
+		exit 0
+		;;
+        d)
+                SRCDIR=${OPTARG}
+                ;;
+        K)
+                KAKADUPATH=${OPTARG}
+                ;;
+        n)
+                SECPERIMG=${OPTARG}
+                ;;
+        r)
+                RESOLUTION=${OPTARG}
+                ;;
+        t)
+                TMPDIR=${OPTARG}
+                ;;
+        \?)
+                echo "Invalid option: -${OPTARG}" >&2
+		usage
+                exit 1
+                ;;
+        :)
+                echo "Option -${OPTARG} requires an argument." >&2
+		usage
+                exit 1
+                ;;
+        esac
+done
+
+#
+# Some sanity check on the parameters follow
+#
+
+if [ ! -d ${SRCDIR} ]
+then
+	echo "Invalid source directory specified." >&2
+	exit 2
+fi
+
+if [ ! -d ${KAKADUPATH} ] || [ ! -x ${KAKADUPATH}/kdu_expand ]
+then
+	echo "Invalid Kakadu directory specified." >&2
+	exit 2
+fi
+
+_RESOLUTION=`echo ${RESOLUTION} | grep -oE '^[[:digit:]]+x[[:digit:]]$'`
+
+if [ -z ${RESOLUTION} ]
+then
+	echo "Invalid resolution specification. It can only contain digits and an x letter separating the two dimensions." >&2
+	exit 2
+fi
+
+_SECPERIMG=`echo ${RESOLUTION} | grep -oE '^[[:digit:]]+$'`
+
+if [ -z ${SECPERIMG} ]
+then
+	echo "Invalid second per image specification. It can only contain digits." >&2
+	exit 2
+fi
+
+# Create temp directory if does not exist
+mkdir -p ${TMPDIR}
+
+#
+# Main loop to iterate over the images in the source directory
+#
+while :
+do
+	for f in `find ${SRCDIR} -type f -regex '.*\.jp2$' | sort`
+	do
+		# Extract JPEG 2000 image
+		env LD_LIBRARY_PATH=${KAKADUPATH} ${KAKADUPATH}/kdu_expand -i ${f} -o ${TMPDIR}/foo.bmp
+
+		# Stream to stdout
+		ffmpeg -loop_input -i ${TMPDIR}/foo.bmp -t ${SECPERIMG} -s ${RESOLUTION} -vcodec libtheora -f ogg -
+
+		# Clean up temporary file
+		rm -f ${TMPDIR}/foo.bmp
+	done
+done
